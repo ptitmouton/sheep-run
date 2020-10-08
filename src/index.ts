2e091155372dcfb6ecb5d0737b96b4ed841b456b
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2020-10-08 15:25:21 +0200

    add help alert

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,24 @@ import { Game } from './engine/game';
 const gameWidth = 1024;
 const gameHeight = 512;
 
-let canvas;
 document.addEventListener('DOMContentLoaded', () => {
-    canvas = document.createElement('canvas');
-    canvas.setAttribute('width', gameWidth);
-    canvas.setAttribute('height', gameHeight);
+    const helpButton = document.createElement('button');
+    helpButton.classList.add('help-button');
+    helpButton.innerText = 'Help';
+    helpButton.addEventListener('click', () => {
+        alert(`
+Move Left: A / ArrowLeft
+Move Up: D / ArrowRight
+Jump: Space
+Attack: ShiftLeft / ShiftRight
+Debug: h
+        `);
+    });
+    document.body.append(helpButton);
+
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('width', String(gameWidth));
+    canvas.setAttribute('height', String(gameHeight));
     document.body.append(canvas);
 
     const game = new Game(canvas);
